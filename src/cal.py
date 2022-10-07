f4a2a148a6d9003b0395462715b3f2b7c16962aa
SeuuungHyeeeon <mcu0102@gmail.com>
2022-10-07 16:42:21 +0900

    [10/7/2022]

---
+++ src/cal.py
@@ -0,0 +1,19 @@
+def plus(x,y):
+  z = x + y
+  return z
+
+def minus(x,y):
+  z = x - y
+  return z
+
+def multiplication(x,y):
+  z = x * y
+  return z
+
+def division(x,y):
+  z = x / y
+  return z
+
+def remainder(x,y):
+  z = x % y
+  return z
